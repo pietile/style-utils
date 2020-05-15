9f77a363ea348679d2431be056f44dd1b53e6c9b
Maksim Milyutin <mmxckk@gmail.com>
2020-05-15 18:44:44 +0700

    Fix tests

---
+++ src/__tests__/transition.test.ts
@@ -0,0 +1,21 @@
+import { transition, cubicTransition } from '../transition';
+
+describe('transition', () => {
+  it('should return valid value', () => {
+    expect(transition({ duration: '225ms', property: 'opacity' })).toBe('opacity 225ms');
+    expect(
+      transition({
+        duration: '225ms',
+        property: 'opacity',
+        delay: '225ms',
+        timingFunction: 'ease',
+      }),
+    ).toBe('opacity 225ms ease 225ms');
+  });
+});
+
+describe('cubitTransition', () => {
+  it('should return valid value', () => {
+    expect(cubicTransition('opacity')).toBe('opacity 235ms cubic-bezier(0.4, 0, 0.2, 1)');
+  });
+});
--- src/transition.ts
+++ src/transition.ts
@@ -1,5 +1,24 @@
-export function cubicTransition(...cssProperties: string[]): string {
-  return cssProperties
-    .map((property) => `${property} 235ms cubic-bezier(0.4, 0, 0.2, 1)`)
+interface TransitionOptions {
+  property: string;
+  duration: string;
+  timingFunction?: string;
+  delay?: string;
+}
+
+export function transition(...transitions: TransitionOptions[]): string {
+  return transitions
+    .map(({ property, duration, timingFunction, delay }) =>
+      [property, duration, timingFunction, delay].filter(Boolean).join(' '),
+    )
     .join(', ');
+}
+
+export function cubicTransition(...cssProperties: string[]): string {
+  return transition(
+    ...cssProperties.map((property) => ({
+      property,
+      duration: '235ms',
+      timingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    })),
+  );
 }
