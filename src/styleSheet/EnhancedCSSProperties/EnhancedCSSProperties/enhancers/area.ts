5fbb8d9793f58701b25af9c7413f1ec3dd91ea04
Maksim Milyutin <mmxckk@gmail.com>
2021-02-15 20:30:03 +0300

    Area improvments

--- src/styleSheet/EnhancedCSSProperties/EnhancedCSSProperties/enhancers/area.ts
+++ src/styleSheet/EnhancedCSSProperties/EnhancedCSSProperties/enhancers/area.ts
@@ -1,20 +1,42 @@
-type AreaOptions =
-  | { vertical: number; horizontal: number }
-  | { top: number; horizontal: number; bottom: number }
-  | { top: number; right: number; bottom: number; left: number };
+type AreaValue = number | string | 'auto';
 
-export function area(value: number | AreaOptions): string {
-  if (typeof value === 'number') {
-    return `${value}px`;
+type AreaOptions =
+  | { vertical: AreaValue; horizontal: AreaValue }
+  | { top: AreaValue; horizontal: AreaValue; bottom: AreaValue }
+  | { top: AreaValue; right: AreaValue; bottom: AreaValue; left: AreaValue }
+  | [top: AreaValue, right: AreaValue, bottom: AreaValue, left: AreaValue]
+  | [top: AreaValue, horizontal: AreaValue, bottom: AreaValue]
+  | [vertical: AreaValue, horizontal: AreaValue]
+  | AreaValue;
+
+function getAreaFromValue(value: AreaOptions): AreaValue[] {
+  if (Array.isArray(value)) {
+    return value;
+  }
+
+  if (typeof value === 'number' || typeof value === 'string') {
+    return [value];
   }
 
   if ('left' in value) {
-    return `${value.top}px ${value.right}px ${value.bottom}px ${value.left}px`;
+    return [value.top, value.right, value.bottom, value.left];
   }
 
   if ('bottom' in value) {
-    return `${value.top}px ${value.horizontal}px ${value.bottom}px`;
+    return [value.top, value.horizontal, value.bottom];
   }
 
-  return `${value.vertical}px ${value.horizontal}px`;
+  return [value.vertical, value.horizontal];
+}
+
+export function area(value: AreaOptions): string {
+  return getAreaFromValue(value)
+    .map((area) => {
+      if (typeof area === 'number') {
+        return `${area}px`;
+      }
+
+      return area;
+    })
+    .join(' ');
 }
